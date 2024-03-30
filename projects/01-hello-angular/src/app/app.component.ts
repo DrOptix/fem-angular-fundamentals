502a5f4993782cdf3b3399c02e90b541edc621e9
Cristian "DrOptix" Dinu <cristian.virgil.dinu@gmail.com>
2024-03-30 13:21:50 +0200

    feat: Solution for 01-hello-angular

--- projects/01-hello-angular/src/app/app.component.ts
+++ projects/01-hello-angular/src/app/app.component.ts
@@ -7,9 +7,16 @@ import { RouterOutlet } from '@angular/router';
   standalone: true,
   imports: [CommonModule, RouterOutlet],
   template: `
-    <h1>If you are reading this...</h1>
-    <p>Things have worked out well! 🎉</p>
+    <ol>
+      <li>Hitman</li>
+      <li>SplinterCell</li>
+      <li>Red Dead Redemtion 2</li>
+    </ol>
   `,
-  styles: ``,
+  styles: `
+    ol {
+      list-style-type: upper-roman;
+    }
+  `,
 })
 export class AppComponent {}
